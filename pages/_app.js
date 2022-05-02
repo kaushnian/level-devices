83dc3cc47c7dd58969fcbd29fb4c6365a1cd30b4
Yevgeny Kaushnian <yevgeny@landlordintegrity.com>
2022-05-02 14:38:36 -0400

    Init the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Level Devices</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="main-container">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
-
-export default MyApp
